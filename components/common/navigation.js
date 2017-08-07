5049919039bd9c964822c8242a9ea7bcee73db75
David Nimmo <dnimmo@gmail.com>
2017-08-07 01:31:21 +0100

    minor update to sign in/out text

--- components/common/navigation.js
+++ components/common/navigation.js
@@ -1,15 +1,19 @@
 import Link from 'next/link'
 import nextConnect from '../../state/store'
 
+const signUpText = 
+  loggedIn => ( 
+    loggedIn 
+      ? 'Log out' 
+      : 'Sign up/in' 
+  )
+
 const navigation = 
   ({loggedIn}) => (
     <nav className='siteNavigation'>
       <Link href='/index'><a>Home</a></Link>
       <Link href='/about'><a>About</a></Link>
-      { loggedIn 
-        ? <Link href='/sign-up'><a>Log out</a></Link>
-        : <Link href='/sign-up'><a>Sign up/in</a></Link>
-      }
+      <Link href='/sign-up'><a>{signUpText(loggedIn)}</a></Link>
       <Link href='/book-demo'><a>Book a demo</a></Link>
     </nav>
   )
